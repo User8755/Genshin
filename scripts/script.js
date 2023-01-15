ad740c49bc3fd9b362f7007c2e371fae91163783
Evgeny Kalinin <user8755@yandex.ru>
2023-01-15 18:02:29 +0500

    добавил список выпавших героев

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,25 @@
 const button = document.querySelector('.banner__button');
 const image = document.querySelector('.banner__image');
 const title = document.querySelector('.banner__hero-name');
+const page = document.querySelector('.page')
+const template = document.querySelector('.template').content;
+const grid = document.querySelector ('.grid');
 
 const randomimg = (list) => {
     const random = Math.floor(Math.random() * list.length);
     return list[random];
 }
+const renderImg = (item) => {
+    const cloneGrid = template.cloneNode(true);
+    cloneGrid.querySelector ('.mimipicture').src = item;
+    return cloneGrid
+};
+
+
 
 button.addEventListener('click', () => {
     const randomhero = randomimg(heroList);
     image.src = randomhero.link;
-    title.textContent = randomhero.hero;
+    setTimeout(() => {title.textContent = randomhero.hero}, 50);
+    grid.prepend(renderImg(randomhero.link))
 });
